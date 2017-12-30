03fc903faaf8aec43689bd90342603acb8b3fc42
Piotr Korlaga <piotrkorlaga@gmail.com>
2017-12-30 16:34:57 +0100

    init repo

---
+++ src/test/java/pl/applantic/cqrs/AppTest.java
@@ -0,0 +1,14 @@
+package pl.applantic.cqrs;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+@Test
+public class AppTest {
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
