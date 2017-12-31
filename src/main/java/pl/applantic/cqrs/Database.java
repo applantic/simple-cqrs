a22fd171222d10d5807588f6c8754947800b4fdf
Piotr Korlaga <piotrkorlaga@gmail.com>
2017-12-31 10:31:26 +0100

    added test

--- src/main/java/pl/applantic/cqrs/Database.java
+++ src/main/java/pl/applantic/cqrs/Database.java
@@ -3,6 +3,10 @@ package pl.applantic.cqrs;
 class Database {
     private WriteModel writeModel = new WriteModel();
 
+    public Database(WriteModel writeModel) {
+        this.writeModel = writeModel;
+    }
+
     public WriteModel getWriteModel() {
         return writeModel;
     }
